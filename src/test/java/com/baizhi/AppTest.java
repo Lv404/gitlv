cb432d78c9c6f599cf7a2b0a5f427b805d52a81c
LV <1475017874@qq.com>
2019-09-19 10:44:50 +0800

    test project作为仓库

---
+++ src/test/java/com/baizhi/AppTest.java
@@ -0,0 +1,25 @@
+package com.baizhi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+
+    public void dummy() {
+        System.out.println("project需要初始化为Git仓库，而不是module");
+        System.out.println("project和module都需要初始化为Git仓库");
+    }
+}
